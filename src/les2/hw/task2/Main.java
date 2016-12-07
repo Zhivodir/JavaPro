8847048c2ce6a6ca43d6eb6ff5943c46480f1b07
zhivodir <dzhivchik@gmail.com>
2016-12-07 11:00:00 +0200

    parser of JSON

---
+++ src/les2/hw/task2/Main.java
@@ -0,0 +1,31 @@
+package les2.hw.task2;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by User on 06.12.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder().create();
+        String result = readFromFile("c:/DevKit/json.txt");
+        Visitka visitka = (Visitka) gson.fromJson(result, Visitka.class);
+        System.out.println(visitka);
+    }
+
+    public static String readFromFile(String path){
+        StringBuilder sb = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))){
+            String text;
+            while((text = reader.readLine()) != null  ) {
+                sb.append(text);
+            }
+        } catch(IOException e){ e.printStackTrace(); }
+        return sb.toString();
+    }
+}
